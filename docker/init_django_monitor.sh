9f675df8f0b266d9e35523968e4e2eaaaea8eac8
mudong <471166308@qq.com>
2017-12-28 16:25:50 +0800

    部署文件2

--- docker/init_django_monitor.sh
+++ docker/init_django_monitor.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo '---------------migrate database main------------------------'
 docker exec -it taileizi python manage.py migrate
+echo '---------------collectstatic------------------------'
+docker exec -it taileizi python manage.py collectstatic
