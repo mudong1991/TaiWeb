21d22510af7c09bfe3d489d2990a1fb56aeccc13
mudong <471166308@qq.com>
2017-12-28 18:14:44 +0800

    部署相关文件

--- docker/build.sh
+++ docker/build.sh
@@ -5,4 +5,4 @@ tar -cf TaiWeb.tar -C ../ ./
 echo "Build mudong/taileizi docker image start ......"
 docker build -t mudong/taileizi ./
 echo "Start container taileizi ......"
-docker run --name taileizi -v /var/www/TaiWeb --net=host -d mudong/taileizi /usr/local/bin/uwsgi --ini uwsgi-taileizi.ini
+docker run --name taileizi -v /var/www/TaiWeb:/var/www/TaiWeb --net=host -d mudong/taileizi /usr/local/bin/uwsgi --ini uwsgi-taileizi.ini
